76b906903bc02cd8deb463ef492e7229ea8fc450
fire <gioihannaochochungta9x@gmail.com>
2020-05-23 15:53:41 +0700

    90%

---
+++ index.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Work</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" style="padding-left: 0;padding-top: 20px" href=""><img src="image/CodeGym.png"
+                                                                                               style="width: 200px"></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Lịch khai giảng <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Chương Trình</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Khóa học</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tin tức & sự kiện</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
+<div class="container">
+    <div class="col-md-12" style="background-image: url('image/tablet.jpg'); height: 350px">
+        <div style="color: midnightblue; padding-left: 50px; padding-top: 100px">
+            <h4>ĐÀO TẠO LẬP TRÌNH</h4>
+            <h1 style="font-size:70px">HIỆN ĐẠI</h1>
+        </div>
+    </div>
+    <div class="col-md-12" style="margin-top: 15px">
+        <div class="row" style="height: 130px; background-color: slategrey">
+            <div class="col-3">
+                <img style="height: 50px;margin-top: 20px" src="image/CodeGym-Logo-small.png" alt="">
+            </div>
+            <div class="col-9" style="color: white">
+                CodeGym ra đời với mục tiêu đào tạo những lập trình viên hiện đại, là chủ lực cung cấp nhân lực chất
+                lượng cao cho ngành công nghiệp, góp phần nâng tầm phát triển ngành phần mềm Việt Nam, tiến kịp tiêu
+                chuẩn quốc tế.
+            </div>
+        </div>
+    </div>
+    <br>
+    <h2 style="text-align: center">Các chương trình đào tạo</h2>
+    <br>
+    <div class="card-group">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card">
+                    <h5 class="card-header">CAREER</h5>
+                    <div class="card-body">
+                        <p class="card-text">Chương trình đào tạo full-stack developer chuyên nghiệp trong vòng 6 tháng
+                            dành cho người học từ đầu.</p>
+                        <a href="#" class="btn btn-primary">Tìm hiểu thêm</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <h5 class="card-header">ACCELERATOR</h5>
+                    <div class="card-body">
+
+                        <p class="card-text">Chương trình đào tạo nhanh để cập nhật công nghệ mới và những lĩnh vực có
+                            nhu cầu tuyển dụng cao.</p>
+                        <a href="#" class="btn btn-primary">Tìm hiểu thêm</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <h5 class="card-header">PREMIUM</h5>
+                    <div class="card-body">
+                        <p class="card-text">Chương trình học tập đặc biệt dành cho lập trình viên đam mê nâng cao tay
+                            nghề và trình độ.Tiếp tục cân bằng card tiếp.</p>
+                        <a href="#" class="btn btn-primary">Tìm hiểu thêm</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <h2 style="text-align: center">Hình ảnh hoạt động</h2>
+    <br>
+    <div class="row">
+        <img class="col-sm-4" src="image/1.jpg" alt="">
+        <img class="col-sm-4" src="image/2.jpg" alt="">
+        <img class="col-sm-4" src="image/3.jpg" alt="">
+    </div>
+    <div class="row" style="margin-top: 10px">
+        <img class="col-sm-4" src="image/4.jpg" alt="">
+        <img class="col-sm-4" src="image/5.jpg" alt="">
+        <img class="col-sm-4" src="image/6.jpg" alt="">
+    </div>
+    <br>
+    <h1 style="text-align: center">Liên hệ</h1>
+    <br>
+    <div class="row">
+        <img class="col-sm-6" src="image/address.png" alt="" style="height: 500px">
+        <form class="col-sm-6">
+            <h2 style="text-align: center">Liên hệ tư vấn</h2>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Họ và tên</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email </label>
+                <input type="email" class="form-control" id="exampleFormControlInput1">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Số điện thoại</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Nội dung</label>
+                <textarea name="" id="" cols="43" rows="3"></textarea>
+            </div>
+            <a href="" class="btn btn-primary" style="margin-left: 200px">LIÊN HỆ</a>
+        </form>
+    </div>
+</div>
+<br>
+<br>
+</body>
+<footer style="background-color: blue; color: white">
+    <div class="row">
+        <div class="col-2">
+            <h3>Giới thiệu</h3>
+            <ul class="list-unstyled">
+                <li><a style="color: white" href="#">Về codegym</a></li>
+            </ul>
+        </div>
+        <div class="col-3">
+            <h3>Chương trình</h3>
+            <ul class="list-unstyled">
+                <li><a style="color: white" href="#">Career</a></li>
+                <li><a style="color: white" href="#">Premium</a></li>
+                <li><a style="color: white" href="#">Accelerator</a></li>
+            </ul>
+        </div>
+        <div class="col-3">
+            <h3>Tài nguyên</h3>
+            <ul class="list-unstyled">
+                <li><a style="color: white" href="#">Blog</a></li>
+                <li><a style="color: white" href="#">Tạp chí</a></li>
+                <li><a style="color: white" href="#">AgileBreackfast</a></li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <h3>Cộng đồng</h3>
+            <ul class="list-unstyled">
+                <li><a style="color: white" href="#">Fanpage</a></li>
+                <li><a style="color: white" href="#">Sự kiện</a></li>
+            </ul>
+        </div>
+    </div>
+    <p style="text-align: center">CodeGym@2018. All rights reserved.</p>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        crossorigin="anonymous"></script>
+</html>
